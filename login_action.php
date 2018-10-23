6ac4cae0792cce5c3dcb286f2ca324c6cd05e4cd
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-23 10:18:43 +0100

    adding

--- login_action.php
+++ login_action.php
@@ -5,11 +5,6 @@
 
 
 
-	$template = new HTML_Template_IT('.'); 
-	$template->loadTemplatefile('login_template.html',true, true); 
-
-	$template->setCurrentBlock("LOGINFORM");
-
 	// ligação à base de dados
 $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 // criar query numa string
@@ -41,42 +36,5 @@ if(nrows>0){
 	$_SESSION['error']='2';
 	header("Location:login.php?email=$email");
 }
-
-
-
-	$template->setVariable('EMAIL', $email);
-
-	$template->setVariable('MESSAGE_HIDDEN',"");
-	$message="";
-
-	switch($error){
-		case 1:
-			$message="Please fill in your name";
-		break;
-		
-		case 2:
-			$message="Please insert a valid email";
-		break;
-
-		case 3:
-			$message="Please make sure your password is filled and the confirmation matches";
-		break;
-
-		case 4:
-			$message="Database error, couldn't create a new user. Please try again later";
-		break;
-
-		case 5:
-			$message="Email is already in use. Please check the email used and, if it really is yours, recover your password";
-		break;
-
-		default:
-		$template->setVariable('MESSAGE_HIDDEN',"hidden");
-
-	}
-	$template->setVariable('MESSAGE', $message);
-	$template->parseCurrentBlock();
-
-	$template->show();
 
 ?>
