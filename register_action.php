5523fcd7654c698cabeb631ed310e92e4c821d2d
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-23 10:22:16 +0100

    adding

--- register_action.php
+++ register_action.php
@@ -47,6 +47,8 @@ $nrows = mysql_num_rows($result);
 	
 if($nrows==0)
 {
+	$password=hash('sha512',$password);
+	$password2=$password;
 	$query = "INSERT INTO users (name,email,created_at,updated_at,password_digest,remember_digest,admin) VALUES ('$name','$email',NOW(),NOW(),'$password','$password2',0)";	
 	$result=@mysql_query($query,$db);
 
