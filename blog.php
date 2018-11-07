7d1013a74fac7b7a85a91f5cc55df2824deacbfb
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-07 15:54:23 +0000

    asd

--- blog.php
+++ blog.php
@@ -18,13 +18,16 @@ $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 
 $message="";
 $action="newblog_action.php";
+echo "1";
 if(isset($_GET['POST_ID'])){
+	echo "2";
 	$action="updateblog_action.php";
 	$postid=$_GET['POST_ID'];
 	$query = "SELECT * FROM microposts WHERE ID=$postid";
 	$result = @ mysql_query($query,$db );
 	$nrows = mysql_num_rows($result); 
 	if($nrows>0){
+		echo "3";
 		$tuple = mysql_fetch_array($result,MYSQL_ASSOC);
 		$message =  $tuple['content'] ;
 	}
