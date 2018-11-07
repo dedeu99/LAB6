3e999e2cd8ddf0bf0acda8840e8da2e53cc0a6a3
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-07 16:11:01 +0000

    asd

--- newblog_action.php
+++ newblog_action.php
@@ -12,7 +12,7 @@ $content=$_POST['message'];
 
 
 $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
-$query = "INSERT INTO microposts  (content,user_id,created_at,updated_at) VALUES ($content,$userid, NOW(),NOW())";
+$query = "INSERT INTO microposts  (content,user_id,created_at,updated_at) VALUES (".$content.",$userid, NOW(),NOW())";
 
 $result = @ mysql_query($query,$db );
 //$nrows = mysql_num_rows($result); 
