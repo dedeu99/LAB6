50577b8ab63c205565dd3709337c7a779bd971ab
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-07 16:07:00 +0000

    msg

--- blog.php
+++ blog.php
@@ -18,20 +18,16 @@ $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 
 $message="";
 $action="newblog_action.php";
-echo "1";
+
 if(isset($_GET['POST_ID'])){
-	echo "2";
 	$action="updateblog_action.php";
 	$postid=$_GET['POST_ID'];
 	$query = "SELECT content FROM microposts WHERE id=$postid";
 	$result = @ mysql_query($query,$db );
 	$nrows = mysql_num_rows($result); 
 	if($nrows>0){
-		echo "3";
 		$tuple = mysql_fetch_array($result,MYSQL_ASSOC);
 		$message =  $tuple['content'] ;
-		var_dump($message);
-		var_dump($tuple['content']);
 	}
 }
 
---
+++ newblog_action.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+include 'db.php';
+session_start();
+if(!isset($_SESSION['name']) || !isset($_SESSION['id'])){
+	header("Location: login.php");
+	die();
+}
+
+$userid=$_SESSION['id'];
+$content=$_POST['content'];
+
+
+$db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
+$query = "INSERT INTO microposts  (content,user_id,created_at,updated_at) VALUES ($content,$userid, NOW(),NOW())";
+$result = @ mysql_query($query,$db );
+//$nrows = mysql_num_rows($result); 
+header("Location:index.php");
+?>
