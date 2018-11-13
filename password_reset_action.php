3bba2662f7c51e0a896fe6e1eab990700a2306ec
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-13 16:59:59 +0000

    asd

--- password_reset_action.php
+++ password_reset_action.php
@@ -21,25 +21,28 @@ if($nrows>0){
 	$tuple = mysql_fetch_array($result,MYSQL_ASSOC);
 	$name=$tuple['name'];
 	$query = "UPDATE users SET reset_digest=$reset_digest, reset_sent_at=$time WHERE email=\"$email\"";
+	$result = @ mysql_query($query,$db );
+	$nrows = mysql_num_rows($result); 
+	if($nrows>0){
+		$msg="Olá Sr.(a) $name
+	Para obter uma nova password clique no link
 
-	$msg="Olá Sr.(a) $name
-Para obter uma nova password clique no link
+	http://all.deei.fct.ualg.pt/~a62362/LAB8/new_password.php?token=$reset_digest
 
-http://all.deei.fct.ualg.pt/~a62362/LAB8/new_password.php?token=$reset_digest
-
-Este link tem a validade de uma hora.
-Se NÃO pediu uma nova password IGNORE este email.
+	Este link tem a validade de uma hora.
+	Se NÃO pediu uma nova password IGNORE este email.
 
 
-Cumprimentos,
-webmaster!
-Página Web: http://intranet.deei.fct.ualg.pt/~a62362/Lab8/
-E-mail: a62362@deei.fct.ualg.pt
-NOTA: Não responda a este email, não vai obter resposta!";
+	Cumprimentos,
+	webmaster!
+	Página Web: http://intranet.deei.fct.ualg.pt/~a62362/Lab8/
+	E-mail: a62362@deei.fct.ualg.pt
+	NOTA: Não responda a este email, não vai obter resposta!";
 
-	mail($email,"Password Reset",$msg);
-	header("Location:  message.php?code=1");
-
+		mail($email,"Password Reset",$msg);
+		header("Location:  message.php?code=1");
+	}else
+		header("Location:  message.php?code=-1");
 }
 else{
 	$_SESSION['error']=3;
